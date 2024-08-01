6c16d43d7b454d69ea7865e29502f5b88a9c531c
Lexa Huang <sixian.duidui@gmail.com>
2024-08-01 13:54:10 +0800

    add validating sales data for post endpoint

--- server/routes/sales.js
+++ server/routes/sales.js
@@ -2,9 +2,27 @@ const express = require('express');
 const router = express.Router();
 const { addData, getData } = require('../lib/salesData'); 
 
+function validateSalesData(salesData){
+    const {date, numSales} = salesData;
+
+    if (!date || new Date(date) > new Date()) {
+        return 'Date cannot be in the future.';
+    }
+
+    if (typeof numSales !== 'number' || numSales < 0 || numSales > 500) {
+        return 'Number of sales must be between 0 and 500.';
+    }
+    
+    return null;
+}
 
 router.post('/', (req, res) => {
   const salesData = req.body.salesData;
+
+  const validationError = validateSalesData(salesData);
+  if (validationError) {
+    return res.status(400).json({ message: validationError });
+  }
 
   addData(salesData);
   console.log(getData());
