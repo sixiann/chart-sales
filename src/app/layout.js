40069139a0abcf42b2dd0fab486ea2476f8dbd03
Lexa Huang <sixian.duidui@gmail.com>
2024-08-01 14:08:25 +0800

    install radix ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme appearance = "light">
+          {children}
+        </Theme>
+      </body>
     </html>
   );
 }
